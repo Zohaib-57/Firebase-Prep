973999de66f3eb90b2a96cf7e22b866f9d93cf04
Your Name <zohaibabbas8557@gmail.com>
2025-02-18 23:38:50 +0500

    authentication process added

--- src/pages/SignUpForm.jsx
+++ src/pages/SignUpForm.jsx
@@ -1,24 +1,42 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { firebaseApp } from "../firebase";
 
 const SignUpForm = () => {
-	const [email,setEmail] = useState("");
-	const [password,setPassword] = useState("");
-	const [error,setError] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
 
+	const auth = getAuth(firebaseApp);
 
-	return(
+	const createUser = (e) => {
+		createUserWithEmailAndPassword(auth, email, password).then((value) => {
+			console.log(value);
+		});
+	};
+
+	return (
 		<div className="signup-page">
 			<label htmlFor="">Email</label>
-			<input type="email" required
-			placeholder="Enter your email" />
-			<label htmlFor="">Password</label>
-			<input type="password"
-			required 
-			placeholder="Enter password"/>
+			<input
+				type="email"
+				required
+				value={email}
+				placeholder="Enter your email"
+				onChange={(e) => setEmail(e.target.value)}
+			/>
 
-			<button>SignUp</button>
+			<label htmlFor="">Password</label>
+			<input
+				type="password"
+				required
+				value={password}
+				placeholder="Enter password"
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+
+			<button onClick={createUser}>SignUp</button>
 		</div>
-	)
-}
+	);
+};
 export default SignUpForm;
